ccf0ca7df0762d41862a15e0df69e2dfc51ca850
AgentSMDJ <sandermae036@gmail.com>
2026-09-03 11:07:44 +0200

    har lavet øvelse 2, (added arrays, object, string, number, boolean) function that logs the array

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
-const cardSettings = {
+
+// opgave 1
+// const cardSettings = {
+//     name: "gisa and geralf",
+
+//     image: "gisa and geralf batteling monsters",
+
+//     types: "creature human wizard",
+
+//     abilities: "when gisa and geralf enters mill 4 cards, once during each of your turns you may play a zombie card from your graveyard",
+
+//     stats: "4 power / 4 toughness",
+
+// }
+
+// console.log(cardSettings.name);
+// console.log(cardSettings.types);
+// console.log(cardSettings.abilities);
+// console.log(cardSettings.stats);
+
+// opgave 2
+const cardSettings2 = {
     name: "gisa and geralf",
 
-    image: "gisa and geralf batteling monsters",
+    image: true,
 
-    types: "creature human wizard",
+    types: ["creature", "Human", "wizard"],
 
-    abilities: "when gisa and geralf enters mill 4 cards, once during each of your turns you may play a zombie card from your graveyard",
+    abilities: { 
+        abilitie1: "when gisa and geralf enters the battlefield mill four cards",
 
-    stats: "4 power / 4 toughness",
+        abilitie2: "once during each of your turns you may cast a zombie spell from your graveyard",
+
+    },
+
+    power: 4,
+
+    toughness: 4,
 
 }
 
-console.log(cardSettings.name);
-console.log(cardSettings.types);
-console.log(cardSettings.abilities);
-console.log(cardSettings.stats);
+console.log(cardSettings2.name);
+
+cardSettings2.types.forEach(function(types) {
+    console.log(types)
+
+});
+
+console.log(cardSettings2.abilities.abilitie1);
+console.log(cardSettings2.abilities.abilitie2);
