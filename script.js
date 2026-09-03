dae341b354b6cd385d1515435959efc833c2d0ea
AgentSMDJ <sandermae036@gmail.com>
2026-09-03 10:52:47 +0200

    added an object to javascrip and console.loged it

---
+++ script.js
@@ -0,0 +1,17 @@
+const cardSettings = {
+    name: "gisa and geralf",
+
+    image: "gisa and geralf batteling monsters",
+
+    types: "creature human wizard",
+
+    abilities: "when gisa and geralf enters mill 4 cards, once during each of your turns you may play a zombie card from your graveyard",
+
+    stats: "4 power / 4 toughness",
+
+}
+
+console.log(cardSettings.name);
+console.log(cardSettings.types);
+console.log(cardSettings.abilities);
+console.log(cardSettings.stats);
